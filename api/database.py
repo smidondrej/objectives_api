3e4885330301c3043be40f33593c2b5f3fa0b949
Ondrej Smid <ondrejsmid33@gmail.com>
2021-10-28 10:34:28 +0200

    db url update

--- api/database.py
+++ api/database.py
@@ -1,9 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./db/data.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://new_user:1234@localhost:5432/ddnn_data"
+# SQLALCHEMY_DATABASE_URL = "postgresql://new_user:1234@localhost:5432/ddnn_data"
+SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
