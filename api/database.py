95211bf1d0fe308448d7b8354276ccff3ac014c8
Ondrej Smid <ondrejsmid33@gmail.com>
2021-10-28 11:04:24 +0200

    db

--- api/database.py
+++ api/database.py
@@ -1,20 +1,15 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
-engine = create_engine(os.getenv("DATABASE_URL"))
-
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./db/data.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://new_user:1234@localhost:5432/ddnn_data"
+SQLALCHEMY_DATABASE_URL = "postgres://ytdvwelerojvla:6615a9c69e4e69b3f0d3cc057b854ce09c1f823aaedbcec2c7320917355daba8@ec2-3-86-169-29.compute-1.amazonaws.com:5432/d56g3dl0rl0at3"
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL
-#     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+    # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
