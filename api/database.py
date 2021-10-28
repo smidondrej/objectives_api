db121969655bc0bf3893bf7336afc307869ed558
Ondrej Smid <ondrejsmid33@gmail.com>
2021-10-28 11:01:26 +0200

    db heroku access update

--- api/database.py
+++ api/database.py
@@ -3,14 +3,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./db/data.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://new_user:1234@localhost:5432/ddnn_data"
-SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-    # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL
+#     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
